38a2154265c4580a0eea5bb316e78e28c0cced57
Anton Basov <anton.iv90@mail.ru>
2021-11-23 15:02:10 +0300

    23.11.2021

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -1,3 +1,21 @@
+def positional_arguments(fn):
+    def wrapper(*args):
+        list_ = []
+        for i in [*args]:
+            if type(i) == int:
+                list_.append(i)
+        if [*args] == list_:
+            print("Все аргументы функции являются типом int")
+        result = fn(*args)
+        return result
+
+    return wrapper
+
+
+@positional_arguments
+def some_func(n, m, s):
+    ...
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    some_func(5, 6, 7)
