3027e3a4a56c04324a35fe173ff64d81ba36401f
Anton Basov <anton.iv90@mail.ru>
2021-11-23 15:40:30 +0300

    23.11.2021

--- Занятие2/Домашние_задания/task3_3/main.py
+++ Занятие2/Домашние_задания/task3_3/main.py
@@ -1,3 +1,22 @@
+def iter_arguments(fn):
+    def wrapper(*args, **kwargs):
+        print(*args)
+        try:
+            iter(*args)
+        except TypeError:
+            print('ff')
+        #if iter(*args) != TypeError and iter({**kwargs}) != TypeError:
+            #print("Все объекты итерируемые")
+        result = fn(*args, **kwargs)
+        return result
+
+    return wrapper
+
+
+@iter_arguments
+def some_func(n, b, m, s):
+    ...
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    some_func(7, 8, m=8, s=9)
