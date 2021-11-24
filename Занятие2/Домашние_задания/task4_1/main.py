8cea5b5c07f15431e302bf52bd89eb725e56d68d
Anton Basov <anton.iv90@mail.ru>
2021-11-24 15:14:52 +0300

    24.11.2021

--- Занятие2/Домашние_задания/task4_1/main.py
+++ Занятие2/Домашние_задания/task4_1/main.py
@@ -2,20 +2,13 @@ def get_distance(point: tuple):
     return ((point[1][0] - point[0][0]) ** 2 + (point[1][1] - point[0][1]) ** 2) ** 0.5
 
 
-def task(points: list) -> list:
+def task(points) -> list:
     return sum(list(map(get_distance, points)))
 
 
 def pairwise(iterable):
     for i in range(len(iterable) - 1):
         yield [iterable[i], iterable[i + 1]]
-
-
-def add_list(pts):
-    list_ = []
-    for pair in pairwise(pts):
-        list_ += [pair]
-    return list_
 
 
 if __name__ == "__main__":
